f45a143e80d711375eee5ae3fa661df0ad43a3c2
Shrey-2303 <shrey.shah2303@gmail.com>
2025-08-26 01:59:27 -0400

    completing the rest of the forward pass, will complete the main self attention later

--- modeling_siglip.py
+++ modeling_siglip.py
@@ -1,7 +1,6 @@
 from typing import Optional, Tuple
 import torch 
 import torch.nn as nn 
-
 class SiglipVisionConfig:
     def __init__(
         self,
@@ -29,18 +28,110 @@ class SiglipVisionConfig:
         self.layer_norm_eps = layer_norm_ep
         self.num_image_tokens = num_image_tokens
     
-# the main vision transformer 
+
+
+
+
+# this is a single layer pipeline for the transformer shown in 2_transformer_arch
+class SUglipEncoderLayer(nn.Module):
+    def __init__(self, config:SiglipVisionConfig):
+        super.__init__()
+        self.embed_dim = config.hidden_size
+        self.self_atn = SiglipAttention(config)
+        self.layer_norm1 = nn.LayerNorm(self.embed_dim, eps = config.layer_norm_eps)
+        self.mlp = SiglipMLP(config)
+        self.layer_norm2 = nn.LayerNorm(self.embed_dim, eps = config.layer_norm_eps)
+        
+        
+    def forward(
+        self,
+        hidden_states: torch.Tensor) -> torch.Tensor:
+        
+        residual = hidden_states   # [B, Num_patches, Embed_dim]
+        
+        hidden_states = self.layer_norm1(residual)                     # [B, num_patches, Embed_dim]
+        hidden_states, _ = self.self_atn(hidden_states=hidden_states)  # [B, num_patches, Embed_dim]
+        hidden_states = residual + hidden_states                       # [B, num_patches, Embed_dim]
+        
+        residual = hidden_states
+        
+        hidden_states = self.layer_norm2(residual)                     # [B, num_patches, Embed_dim]
+        hidden_states = self.mlp(hidden_states)                        # [B, num_patches, Embed_dim]
+        hidden_states = residual + hidden_states                       # [B, num_patches, Embed_dim]
+        
+        return hidden_states
+        
+
+
+
+class SiglipVisionEmbeddings(nn.Module):
+    def __init__(self,config: SiglipVisionConfig):
+        super.__init__()
+        self.config = config
+        self.embed_dim = config.hidden_size
+        self.image_size = config.image_size
+        self.patch_size = config.patch_size
+        
+        # basically we want to extract first level info using convolution on these 16 patches without overlap so the kernal is so big.
+        self.patch_embedding = nn.Conv2d(
+            in_channels=config.num_channels,
+            out_channels=config.hidden_size,
+            kernel_size=config.patch_size,
+            stride = config.patch_size,
+            padding = "valid",  # no padding
+        )
+        
+        self.num_patches = (self.image_size//self.patch_size)**2
+        self.num_positions = self.num_patches
+        self.position_embedding = nn.Embedding(self.num_positions,self.embed_dim)
+        
+        # this below registers the postiions of each extracted path feature (0,1,2,3,4...)
+        self.register_buffer(
+            "position_ids",
+            torch.arange(self.num_patches).expand((1,-1)),
+            persistent=False,
+        )
+        
+        
+    def forward(self, pixel_values:torch.FloatTensor) -> torch.Tensor:
+        B,C,H,W = pixel_values.shape
+
+        # Basically feature extraction by a normal convolution,  [B,C,H,W] -> [B, embed_dim, H_new, W_new]
+        patch_embeds = self.patch_embedding(pixel_values)        
+        
+        # Flattening it for 1D embeddings,  [B, embed_dim, H_new, W_new] -> [B, embed_dim, num_patches]  (num_patches = H_new * W_new)
+        embeddings = patch_embeds.flatten(2)
+
+        # concat with positional info
+        embeddings = embeddings + self.position_embedding(self.position_ids)
+        
+        return embeddings
+    
+    
+    
+    
+    
+# the main vision transformer pipeline 
 class SiglipVisionTransformer(nn.Module):
     def __init__(self, config = SiglipVisionConfig):
         super.__init__()
         self.config = config
         embed_dim = config.hidden_size
         
+        # define embedding extractino, transformer forward pass and final layer norm 
         self.embeddings = SiglipVisionEmbeddings(config)
         self.encoder = SiglipEncoder(config)
         self.post_layernorm = nn.LayerNorm(embed_dim, eps = config.layer_norm_eps)
         
-    
+    def forward(self, pixel_values: torch.Tensor) -> torch.Tensor:
+        # [B,C,H,W] -> [B,Num_patches,Embed_dim]
+        
+        hidden_states = self.embeddings(pixel_values)   # basically extract patches and embed them
+        last_hidden_state = self.encoder(input_embeds = hidden_states)  # this is going to be the main list of transformer layers
+        last_hidden_state = self.post_layernorm(last_hidden_state)
+        
+        return last_hidden_state
+        
     
 # pass to the main vision transformer
 class SiglipVisionModel(nn.Module):
